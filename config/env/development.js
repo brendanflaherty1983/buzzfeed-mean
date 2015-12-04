cd6687890e4b10400404f7fee5c7106e845953c0
brendanflaherty1983 <brendanflaherty1983@gmail.com>
2015-12-04 11:34:11 -0600

    Added gmail functionality

--- config/env/development.js
+++ config/env/development.js
@@ -66,12 +66,12 @@ module.exports = {
     sandbox: true
   },
   mailer: {
-    from: process.env.MAILER_FROM || 'MAILER_FROM',
+    from: process.env.MAILER_FROM || 'buzzfeedmean@gmail.com',
     options: {
-      service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+      service: process.env.MAILER_SERVICE_PROVIDER || 'Gmail',
       auth: {
-        user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-        pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+        user: process.env.MAILER_EMAIL_ID || 'buzzfeedmean@gmail.com',
+        pass: process.env.MAILER_PASSWORD || 'c8QUc8uh'
       }
     }
   },
